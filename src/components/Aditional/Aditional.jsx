0ff27c5e9c25ac188bcc0bc4e58cf59159cec072
Iryna <1017imiia@gmail.com>
2024-03-03 00:34:25 +0200

    add scroll

--- src/components/Aditional/Aditional.jsx
+++ src/components/Aditional/Aditional.jsx
@@ -1,8 +1,17 @@
 import { useLocation } from 'react-router-dom';
 import { List, AdditionalWrp, LinkInfo, LineDiv } from './Aditional.stayled';
+import { animateScroll as scroll } from 'react-scroll';
 
 export const AdditionalInfo = () => {
   const location = useLocation();
+
+  const scrollPage = () => {
+    const halfPageHeight = window.innerHeight / 2;
+    scroll.scrollMore(halfPageHeight, {
+      duration: 500,
+      smooth: true,
+    });
+  };
 
   return (
     <AdditionalWrp>
@@ -10,12 +19,16 @@ export const AdditionalInfo = () => {
         <h3>Additional information</h3>
         <List>
           <li>
-            <LinkInfo to="cast" state={{ from: location }}>
+            <LinkInfo to="cast" state={{ from: location }} onClick={scrollPage}>
               Cast
             </LinkInfo>
           </li>
           <li>
-            <LinkInfo to="reviews" state={{ from: location }}>
+            <LinkInfo
+              to="reviews"
+              state={{ from: location }}
+              onClick={scrollPage}
+            >
               Reviews
             </LinkInfo>
           </li>
